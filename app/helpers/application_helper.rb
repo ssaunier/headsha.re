359f2de89719653a83977f57a5e5118a7d424430
Sebastien Saunier <seb@saunier.me>
2014-02-07 12:37:46 +0100

    More foundation

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,5 @@
 module ApplicationHelper
+  def active_if(method)
+    params[:action] == method.to_s ? 'active' : nil
+  end
 end
--- app/models/share.rb
+++ app/models/share.rb
@@ -1,3 +1,5 @@
 class Share < ActiveRecord::Base
   belongs_to :user
+  validates_presence_of :content_url, :header_url, :header_content
+  default_scope order('created_at DESC')
 end
