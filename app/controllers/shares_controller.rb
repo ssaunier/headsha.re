d1b89eefeaa0cd6ca2dcfca5c55ebc9dfb176024
Sebastien Saunier <seb@saunier.me>
2014-02-07 12:21:05 +0100

    Add Foundation

--- app/controllers/shares_controller.rb
+++ app/controllers/shares_controller.rb
@@ -1,6 +1,6 @@
 class SharesController < ApplicationController
   before_action :set_share, only: [:public, :show, :edit, :update, :destroy]
-  skip_before_action :authenticate_user!
+  skip_before_action :authenticate_user!, only: :public
 
   def public
     render :layout => false
