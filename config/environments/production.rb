278194b997eaf880bd7ab7b7bb80524b17e0b536
Sebastien Saunier <seb@saunier.me>
2014-03-04 19:41:04 +0100

    Add default host for emails

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -77,4 +77,6 @@ Headshare::Application.configure do
 
   # Use default logging formatter so that PID and timestamp are not suppressed.
   config.log_formatter = ::Logger::Formatter.new
+
+  config.default_url_options = { :host => "www.headsha.re" }
 end
